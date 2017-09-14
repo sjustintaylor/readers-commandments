a14d361568587652a569ff015efff4c5c49deab2
darthbobcat <s.justin.taylor@gmail.com>
2017-09-14 17:50:33 +1000

    ui draft done

--- js/site.js
+++ js/site.js
@@ -1,3 +1,23 @@
+// Implement new day, add item and cross out item
+var list = {
+  name: "Justin",
+  transfer: FALSE,
+  big/medium/small:{
+    datestamp: ["task", boolStatus]
+  }
+  newDay: function () {
+    if transfer = true {
+      clear all true status tasks.
+    } else {
+      clear all tasks
+    }
+  }
+  addTask:
+  toggleStatus:
+  saveList: {
+    save to local storage
+  }
+}
 
 // $( document ).ready(function(){
 //
