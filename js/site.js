9a672bca373cc61a6672b84914243361a7b41ef2
darthbobcat <s.justin.taylor@gmail.com>
2017-09-24 17:26:43 +1000

    ui edited

--- js/site.js
+++ js/site.js
@@ -1,24 +1,29 @@
 // Implement new day, add item and cross out item
 var list = {
-  name: "Justin",
-  transfer: FALSE,
-  big/medium/small:{
-    datestamp: ["task", boolStatus]
-  }
-  newDay: function () {
-    if transfer = true {
-      clear all true status tasks.
-    } else {
-      clear all tasks
-    }
-  }
-  addTask:
-  toggleStatus:
-  saveList: {
-    save to local storage
+  name: 'Justin',
+  taskList: {
+    // datestamp: [size, 'task description', check status]
+    // size is the same as the section ids (big small medium)
+    // check status is true or false, true is completed
+  },
+  // Atomic Methods listed here only operate on this object
+  addToTaskList: function (size, taskDescription) {
+    // Creates a task with the specified properties and adds it to the task list
+  },
+  toggleTaskStatus: function (taskID) {
+    // Inverts the check status property of the specified task
+  },
+  clearTaskList: function (transfer) {
+    // Clears the task list totally, or just the tasks with check status of true, depending on the value of transfer
+  },
+  setName: function (name) {
+    this.name = name // sanitize input?
   }
 }
 
-// $( document ).ready(function(){
-//
-// });
+$(document).ready(function() {
+  $('.header-title').text('Hello ' + list.name)
+  $('#add-task-big').on('click', function () {
+    $('#big-drawer').slideToggle("normal");
+  })
+});
